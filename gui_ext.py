5a8adac81b6e3e9bcce7ce9373e572c761398e01
Tristan Giga <tristan.giga@free.fr>
2023-01-27 21:57:19 +0100

    Added method choice

--- gui_ext.py
+++ gui_ext.py
@@ -2,7 +2,16 @@ from tkinter import *
 from tkinter import ttk
 from ops import *
 
-def compute(label, message_entry, key_entry, method):
+def compute(label, message_entry, key_entry, method_option, action_option):
     '''Détermine la string chiffrée ou déchiffrée et l'affiche'''
-    message, key = message_entry.get(), key_entry.get()
-    label.config(text=f"{encrypt_cesar(message, (int(key) if key and key.isnumeric() else 0))}")
+    msg, key = message_entry.get(), key_entry.get()
+    label.config(text=f"{method_comp(method_option.get(), msg, key)}")
+    
+def method_comp(method, msg, key):
+    if method == "Cesar":
+        return encrypt_cesar(msg, (int(key) if key and key.isnumeric() else 0))
+    elif method == "ROT13":
+        return encrypt_ROT13(msg)
+    elif method == "Vigenere":
+        return encrypt_vigenere(msg, key)
+    return encrypt_polybius(msg)
--- main.py
+++ main.py
@@ -8,29 +8,42 @@ from gui_ext import *
 # Les implémentations se trouvent dans le module ops.py
 
 
-#Window init
+#Fenêtre titrée
 root = Tk()
 ttk.Label(root, text="NSIEncryption").grid(row=0)
 
 ttk.Label(root, text="Message").grid(row=1)
 ttk.Label(root, text="Key").grid(row=2)
 
-
+# Zone de saisie du message
 message = ttk.Entry(root)
 message.grid(row=1, column=1)
 
+# Zone de saisie de la clé (0 si non renseignée ou si non numérique)
 key = ttk.Entry(root)
 key.grid(row=2, column=1)
 
-methods = ['Cesar', 'ROT13', 'Vigenère', 'Polybius']
-tk_method = StringVar(root)
+# Menu déroulant des méthodes
+methods = ['Cesar', 'ROT13', 'Vigenere', 'Polybius']
+tk_method = StringVar()
 tk_method.set(methods[0])
-
 method = OptionMenu(root, tk_method, *methods)
 method.grid(row = 10)
 
-ttk.Button(root, text="Encrypt", command=lambda: compute(output, message, key, "Cesar")).grid(row=3)
+# Menu déroulant des actions (chiffrer/déchiffrer)
+actions = ['Encrypt', 'Decrypt']
+tk_action = StringVar()
+tk_action.set(actions[0])
+action = OptionMenu(root, tk_action, *actions)
+action.grid()
+
+# Bouton d'action (chiffrer/déchiffrer)
+ttk.Button(root, text="Encrypt", command=lambda: compute(output, message, key, tk_method, tk_action)).grid(row=3)
+
+# Résultat
 output = ttk.Label(root, text="")
 output.grid(row=4)
+
+ttk.Button(root, text="Quit", command=root.destroy).grid(sticky="s")
 
 root.mainloop()
